e0d99aadb5de66a6fefff7870b90b27a1af15f0d
Mariya Tarasyuk <t.marichka21@gmail.com>
2026-06-11 01:34:29 +0300

    Fix duplicate express declaration

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
-const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 
